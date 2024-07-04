fd808de14d9b8879217c2f27eac7474ac8b8a7c8
wuguangyu <1849535067@qq.com>
2024-07-05 01:14:09 +0800

    hot-fix第一次修改

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("第二次修改");
+        System.out.println("hot-fix第一次修改");
     }
 }
